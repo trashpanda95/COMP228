72ffe296d125299356a02733694376da90734acb
trashpanda95 <aubrian230@gmail.com>
2018-07-20 02:42:39 -0400

    Add files via upload

---
+++ BrianAu_COMP228Lab04/src/exercise1/Application.java
@@ -0,0 +1,12 @@
+/*
+ * Created By: Brian Au
+ * Student ID: 300962933
+ * S2018 - COMP 228 - Programming 2 - Assignment 4 - Lab04
+ * Wallace Balaniuc
+ *
+ */
+
+package exercise1;
+
+public class Application {
+}
---
+++ BrianAu_COMP228Lab04/src/exercise1/GUI.java
@@ -0,0 +1,17 @@
+/*
+ * Created By: Brian Au
+ * Student ID: 300962933
+ * S2018 - COMP 228 - Programming 2 - Assignment 4 - Lab04
+ * Wallace Balaniuc
+ *
+ */
+
+package exercise1;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class GUI extends JFrame{
+
+
+}
